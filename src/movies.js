dd1093803037759cf78347a071b3abe792898c8f
Eric Honingh <erichoningh@codedazur.com>
2022-02-03 10:43:15 +0100

    3 things done

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,41 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(movies) {
+  const getAllDirectorsExtra = movies.map((movie) => {
+    return movie.director;
+  });
+  return getAllDirectorsExtra;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+  const howManyMoviesGenre = movies.filter((movie) => {
+    if (
+      movie.director.includes('Steven Spielberg') &&
+      movie.genre.includes('Drama')
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  return howManyMoviesGenre.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+  const scoresAverageExtra = movies.reduce((acc, movie) => {
+    return acc + movie.score / movies.length;
+  }, 0);
+  // console.log(Number.parseFloat(scoresAverageExtra).toFixed(2))
+  const returnsString = Number(scoresAverageExtra);
+  // console.log(returnsString);
+  const returnsNumber = +returnsString.toFixed(2);
+  // console.log(returnsNumber);
+  return returnsNumber;
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore() {}
@@ -24,8 +52,6 @@ function turnHoursToMinutes() {}
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg() {}
 
-
-
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
@@ -37,6 +63,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     orderAlphabetically,
     turnHoursToMinutes,
-    bestYearAvg,
+    bestYearAvg
   };
 }
